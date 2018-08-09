2b86ce80435863353930ababd2c8d9e0a5dffd10
Eunbit Kim <eunbitkim90@gmail.com>
2018-08-09 11:19:06 +0100

    first commit

---
+++ spec/PlaneSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Plane',function(){
+  var plane;
+  var airport;
+  beforeEach(function(){
+    airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
+  });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+  it('can clear planes for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
+  });
+});
